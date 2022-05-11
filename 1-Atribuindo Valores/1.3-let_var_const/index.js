800238ceb231add62033d94de454e6b54ac6d45f
HaruoTaks <haruo.takahashi@outlook.com>
2022-05-11 16:40:20 -0300

    Bloco de aulas atribuindo valores finalizado

---
+++ 1-Atribuindo Valores/1.3-let_var_const/index.js
@@ -0,0 +1,6 @@
+/* 
+não hoisting = Necessário pelo menos declarar a váriavel antes de utilizar
+var{ Escopo = global ou local; Pode redeclarar = sim; pode reatribir = sim; hoisting = sim }  
+const{ Escopo = bloco; Pode redeclarar = não; pode reatribir = não; hoisting = não }
+let{ Escopo = bloco; Pode redeclarar = não; pode reatribir = sim; hoisting = não }
+*/
