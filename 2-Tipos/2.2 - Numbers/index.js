f5d528e8e7f3e160f3b6447ddf41b5f5823e39f6
HaruoTaks <haruo.takahashi@outlook.com>
2022-05-13 14:35:10 -0300

    2.2 - Numbers

---
+++ 2-Tipos/2.2 - Numbers/index.js
@@ -0,0 +1,12 @@
+let num = 100;
+
+let Soma = num + 2
+console.log(Soma);
+
+let fiveByThree = 5/3;
+// Arredonda o resulta para baixo
+console.log(Math.floor(fiveByThree));
+// Arrendonda o resultado para cima
+console.log(Math.ceil(fiveByThree));
+
+
