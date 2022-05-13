e29f54ccc9d682b0a34509b98c9dc3069b15064f
HaruoTaks <haruo.takahashi@outlook.com>
2022-05-13 15:42:59 -0300

    2.3 - Booleans

---
+++ 2-Tipos/2.3 - Booleans/index.js
@@ -0,0 +1,8 @@
+// Booleano serve para validar se é verdadeiro ou falso
+
+// Ex1: resultado esperado falso
+let validation = 3 == 0;
+console.log(validation);
+
+// Ex2: resultado esperado verdadeiro
+console.log(!validation);
