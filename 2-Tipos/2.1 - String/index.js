61d7da206aae98d958ad983071601415e9ebb39a
HaruoTaks <haruo.takahashi@outlook.com>
2022-05-13 13:54:35 -0300

    2.1 - String

---
+++ 2-Tipos/2.1 - String/index.js
@@ -0,0 +1,22 @@
+/* let um = "um";
+console.log(um); */
+
+// String 
+let nome = "João";
+let sobrenome = "Pedro";
+
+// Concatenado
+//let concatenado = nome.concat(sobrenome);
+
+// Concatenado com espaço
+// let concatenado = nome + " " + sobrenome;
+
+// Concatenado pulando linha
+// let concatenado = nome + "\n" + sobrenome;
+
+// Concatenado com espaço nova forma
+let concatenado = `${nome} ${sobrenome}`;
+console.log(concatenado);
+
+/* let letra = nome[1];
+console.log(letra); */
