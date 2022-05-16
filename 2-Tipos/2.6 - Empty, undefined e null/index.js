729fa9d039df296dde9c543ed59ba7dbdfb3754a
HaruoTaks <haruo.takahashi@outlook.com>
2022-05-16 17:31:16 -0300

    2.6 - Empty, undefined e null

---
+++ 2-Tipos/2.6 - Empty, undefined e null/index.js
@@ -0,0 +1,19 @@
+// Empty
+// Variavel foi declarada mas sem valor
+let pera = "";
+console.log("Empty(Pêra): " + pera);
+console.log("Valor é verdadeiro? " + (pera === true));
+console.log("Valor é falso? " + (pera === false));
+//Null
+// quando você quer que o valor seja nulo
+let maca = null;
+console.log("Null(maça): " + maca);
+console.log("Valor é verdadeiro? " + (maca === true));
+console.log("Valor é falso? " + (maca === false));
+// Undefined
+// Não existe nada
+//ex1
+let abacaxi;
+console.log("Undefined(abacaxi): " + abacaxi);
+console.log("Valor é verdadeiro? " + (abacaxi === true));
+console.log("Valor é falso? " + (abacaxi === false));
